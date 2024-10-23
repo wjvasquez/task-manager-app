850fcc022193a2e6e8bb685d58c0705c31490071
wjvasquez <wilmanvasdez@gmail.com>
2024-10-23 17:14:06 -0500

    Add form to create tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,24 @@ class TasksController < ApplicationController
   def show
     @task = current_user.tasks.find(params[:id])
   end
+
+  def new
+    @task = current_user.tasks.build
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+
+    if @task.save
+      redirect_to @task, notice: "Task was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date, :status)
+  end
 end
