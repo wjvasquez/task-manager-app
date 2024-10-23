7ab2371c6765bd865a0fb659eab61bc6119b555a
wjvasquez <wilmanvasdez@gmail.com>
2024-10-23 11:59:00 -0500

    Add index page to show list of tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,7 @@
+class TasksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @tasks = current_user.tasks
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "tasks#index"
+
+  resources :tasks
 end
