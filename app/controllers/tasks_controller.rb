4073d812b7500640b7633a69ff531eb7cd0858b1
wjvasquez <wilmanvasdez@gmail.com>
2024-10-23 20:39:03 -0500

    Add edit task feature
    - Implement edit and update actions in TaskController to allow users to update tasks.
    - Create 'edit' view with a form to modify task details (title, description, due date).
    - Add validations and error handling for task updates.
    - Link 'Edit task' button from the task show page.
    - Fix button name from new page to be general.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,20 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def update
+    @task = current_user.tasks.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task, notice: "Task was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def task_params
